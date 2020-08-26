955988aba5e42ba06369d529d2b3bbe01f3ae00a
Glauber Brack <glauber@brack.com.br>
2020-08-26 00:17:37 -0300

    importing logo to application on main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Image, Text,  } from 'react-native';
+
 import Input from '../../components/Input';
+
+import { styles } from './styles';
+
+import logo from '../../assets/images/logo.png';
 
 export default function App() {
   const [cep, setCep] = useState('');
@@ -10,41 +15,33 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Input
-        mask="cep"
-        value={cep}
-        maxLength={9}
-        placeholder="Enter your Zip Code"
-        placeholderTextColor="#212121"
-        inputMaskChange={(text: string) => setCep(text)}
-      />
+        <Image source={logo} />
+        <Input
+            mask="cep"
+            value={cep}
+            maxLength={9}
+            placeholder="Enter your Zip Code"
+            placeholderTextColor="#212121"
+            inputMaskChange={(text: string) => setCep(text)}
+        />
 
-      <Input
-        mask="phone"
-        value={phone}
-        maxLength={14}
-        placeholder="What's your phone?"
-        placeholderTextColor="#212121"
-        inputMaskChange={(text: string) => setPhone(text)}
-      />
+        <Input
+            mask="phone"
+            value={phone}
+            maxLength={14}
+            placeholder="What's your phone?"
+            placeholderTextColor="#212121"
+            inputMaskChange={(text: string) => setPhone(text)}
+        />
 
-      <Input
-        mask="currency"
-        value={currency}
-        maxLength={14}
-        placeholder="Enter the value"
-        placeholderTextColor="#212121"
-        inputMaskChange={(text: string) => setCurrency(text)}
-      />  
+        <Input
+            mask="currency"
+            value={currency}
+            maxLength={14}
+            placeholder="Enter the value"
+            placeholderTextColor="#212121"
+            inputMaskChange={(text: string) => setCurrency(text)}
+        />  
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#082A44',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/pages/Main/styles.tsx
+++ src/pages/Main/styles.tsx
@@ -1,6 +1,13 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#082A44',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    
     logo: {
         
     }
