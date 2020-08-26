0799c6df82f79a6fb2b698f9b5a7beff157a67d8
Glauber Brack <glauber@brack.com.br>
2020-08-25 22:58:31 -0300

    interface inputprops on input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { StyleSheet, View, Text, TextInput } from 'react-native';
 
 interface InputProps {
-
+    mask: 'cep' | 'phone' | 'currency',
+    inputMaskChange: any;
 }
 
-const Input: React.FC<InputProps> = () => {
+const Input: React.FC<InputProps> = ({ mask, inputMaskChange, ...rest }) => {
   return (
       <>
-        <TextInput style={styles.input}>Componente de Input</TextInput>
+      
+        <TextInput
+            style={styles.input}
+            {...rest}        
+        />
+
       </>
   );
 }
@@ -17,10 +23,10 @@ export default Input;
 
 const styles = StyleSheet.create({
     input: {
-        height: 54,
+        height: 44,
         width: 200,
         backgroundColor: '#fff',
-        borderRadius: 8,
+        borderRadius: 4,
         paddingHorizontal: 16,
         marginTop: 4,
         fontSize: 14
